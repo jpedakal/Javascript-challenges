b0d9bd142d9ba897a897759710f5ca01c6c58e52
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-06-21 21:08:52 +0530

    Update basic1.js

--- basic1.js
+++ basic1.js
@@ -1,5 +1,5 @@
 // Triple add function
-function tripleAdd(num1) {
+/*function tripleAdd(num1) {
     console.log(num1)
     return function (num2) {
         return function (num3) {
@@ -8,4 +8,13 @@ function tripleAdd(num1) {
     }
 }
 
-tripleAdd(10)(20)(30);
+tripleAdd(10)(20)(30); */
+
+/***********************************************************************************************/
+// Output of below code
+
+for(var i=0;i<4;i++){
+   setTimeout(() => {
+       console.log('i value '+i);
+   }, 3000);
+}
