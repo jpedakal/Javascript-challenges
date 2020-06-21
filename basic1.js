d680fe3e481a53fac4d1254568189c95bd911e08
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-06-21 21:14:03 +0530

    Update basic1.js

--- basic1.js
+++ basic1.js
@@ -19,3 +19,12 @@ for(var i=0;i<4;i++){
        console.log('i value '+i);
    }, 1000);
 }
+
+/************************************************************************************************/
+// Find the length of the function
+function addNum(a,b,c){
+    var sum= a+b;
+    return sum;
+}
+
+console.log(addNum.length)
