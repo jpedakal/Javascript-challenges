150904d52959a8f07d914ff42dc8571bc55da150
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-06-22 20:32:45 +0530

    Update basic1.js

--- basic1.js
+++ basic1.js
@@ -39,4 +39,24 @@ console.log(addNum.length)
 
 */
 
-//
+// Write a function for closures concept. Access variables from outer function variable and global variable.
+
+const globalVariable= 'global var';
+
+function outerFunc(param1){
+    const variable= 'var one';
+
+    function innerFunc(param2){
+        const variable2= 'var two';
+
+        console.log('globalVariable:', globalVariable);
+        console.log('variable1', variable);
+        console.log('variable2', variable2);
+        console.log('param1', param1);
+        console.log('param2', param2);
+    }
+
+    innerFunc('param one');
+}
+
+outerFunc('param two');
