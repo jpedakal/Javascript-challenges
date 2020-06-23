93514de3a3f0dcdba7ce5989bd9d62542f93e2a8
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-06-23 21:02:12 +0530

    Update basic1.js

--- basic1.js
+++ basic1.js
@@ -227,3 +227,8 @@ console.log(1 < 2 < 3);
 console.log(3 > 2 > 1);
 
  */
+
+console.log("0 || 1 = "+(0 || 1));
+console.log("1 || 2 = "+(1 || 2));
+console.log("0 && 1 = "+(0 && 1));
+console.log("1 && 2 = "+(1 && 2));
