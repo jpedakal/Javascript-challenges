58e317237dd67d40c099c39356e1c298857cfd18
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-06-23 14:58:00 +0530

    Update basic1.js

--- basic1.js
+++ basic1.js
@@ -211,3 +211,13 @@ var stoleSecretIdentity = hero.getSecretIdentity;
 
 console.log(stoleSecretIdentity());
 console.log(hero.getSecretIdentity());
+
+/**
+ Explanation
+ --------------------------------------------
+ The first console.log prints undefined because we are extracting the method from the hero object, so stoleSecretIdentity() is being invoked in the global context (i.e., the window object) where the _name property does not exist.
+
+One way to fix the stoleSecretIdentity() function is as follows:
+
+var stoleSecretIdentity = hero.getSecretIdentity.bind(hero);
+ */
