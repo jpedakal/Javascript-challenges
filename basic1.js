c90d44839063c5edabe5325ebbded364932cbbcc
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-06-24 11:19:52 +0530

    Update basic1.js

--- basic1.js
+++ basic1.js
@@ -272,7 +272,6 @@ console.log(typeof undefined == typeof NULL);
 //  The expression will be evaluated to true, since NULL will be treated as any other undefined variable.
 //  Note: JavaScript is case-sensitive and here we are using NULL instead of null.
 
-*/
 // Write the output of below code
 var b = 1;
 function outer(){
@@ -285,3 +284,4 @@ function outer(){
     inner();
 }
 outer();
+*/
