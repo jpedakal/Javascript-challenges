419ffec6f30e76832f0d91cc2f7503b8849ddcfe
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-06-24 11:05:32 +0530

    Update basic1.js

--- basic1.js
+++ basic1.js
@@ -267,3 +267,8 @@ arr.unshift('strt');
 console.log(arr);
 
 */
+
+console.log(typeof undefined == typeof NULL);
+
+//  The expression will be evaluated to true, since NULL will be treated as any other undefined variable.
+//  Note: JavaScript is case-sensitive and here we are using NULL instead of null.
