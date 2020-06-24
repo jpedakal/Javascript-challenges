c67b22b7ef4f8337b6fcced3976196125ceb0c58
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-06-24 11:19:33 +0530

    Update basic1.js

--- basic1.js
+++ basic1.js
@@ -273,3 +273,15 @@ console.log(typeof undefined == typeof NULL);
 //  Note: JavaScript is case-sensitive and here we are using NULL instead of null.
 
 */
+// Write the output of below code
+var b = 1;
+function outer(){
+   	var b = 2
+    function inner(){
+        b++;
+        var b = 3;
+        console.log(b)
+    }
+    inner();
+}
+outer();
