8d5ef9a4d0409fd83ec2e936667abf5a0b39f97d
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-06-24 12:29:08 +0530

    Update basic1.js

--- basic1.js
+++ basic1.js
@@ -313,5 +313,15 @@ function foo(){
 
 foo();
 */
-var a=10;
-console.log(void a,a);
+function doSomething() {    // note: `APP.ready` is provided by our application    
+if (!APP.ready) {        // try again later        
+    return void setTimeout( doSomething,100 );    
+}    
+var result;    
+// do some other stuff    
+return result;
+}
+// were we able to do it right away?
+if (doSomething()) 
+{    // handle next tasks right away
+}
