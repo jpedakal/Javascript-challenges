ff0f7fc180ef880db847534f0919432be5d85c9a
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-06-27 10:13:28 +0530

    Update basic1.js

--- basic1.js
+++ basic1.js
@@ -145,7 +145,7 @@ console.log("array 1: length=" + arr1.length + " last=" + arr1.slice(-1));
 console.log("array 2: length=" + arr2.length + " last=" + arr2.slice(-1));
 
 
-
+// Write the output of code
 console.log(1 + "2" + "2");
 console.log(1 + +"2" + "2");
 console.log(1 + -"1" + "2");
