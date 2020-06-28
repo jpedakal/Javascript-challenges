e5b7d60d0ee34e95bbc4be621335cb855881be83
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-06-28 10:50:03 +0530

    Update basic1.js

--- basic1.js
+++ basic1.js
@@ -409,7 +409,6 @@ console.log(a[2][0]);
 
 
 // String concat
-
 var str= "jaya";
 var strconcat= str.concat("krishna");
 console.log(strconcat);
@@ -418,8 +417,6 @@ console.log(strconcat);
 var arr= ["j","a","y","a"];
 var arr1= arr.concat(["k","r","i","s","h","n","a"]);
 console.log(arr1);
-
-*/
 
 // Find the output of below code
 
