53efa0b88ead39989dda19eb07f3cdae58921476
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-06-28 10:48:54 +0530

    Update basic1.js

--- basic1.js
+++ basic1.js
@@ -389,7 +389,7 @@ var maxnum2=  Math.pow(2, 969);
 console.log(maxnum2);            // 1.7976931348623157e+308
 
 
-//  Samples about infinity
+// Samples about infinity
 console.log(Infinity/Infinity);  // NaN
 console.log(10000/ Infinity); // 0
 console.log(-100000/ Infinity); // -0
