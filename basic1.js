81b68d9d68ce9e900e18ccc5cdedf7966cf9dd7e
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-07-06 12:13:43 +0530

    Update basic1.js

--- basic1.js
+++ basic1.js
@@ -607,7 +607,7 @@ console.log(a >= b);     // true
 /*
 Because the spec says for a <= b, it will actually evaluate b < afirst, and then negate that 
 result. Since b < a is also false, theresult of a <= b is true.
-*/
+
 
 //Find the output of below code
-
+*/
