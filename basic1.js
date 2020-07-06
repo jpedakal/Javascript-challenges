afe4cad7dbe6243076e12f9a2d6992b3293de63d
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-07-06 12:14:35 +0530

    Update basic1.js

--- basic1.js
+++ basic1.js
@@ -544,7 +544,6 @@ console.log(JSON.stringify(function(){}));    // undefined
 console.log(JSON.stringify([1,undefined,function(){},4]));    // [1, null, null, 4]
 console.log(JSON.stringify({a: 4, b: function(){}}));     // "{"a":4}"
 
-
 // Find the output of below code
 var obj= {};
 
