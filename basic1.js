408d1849deab0c5b2a6fbf28150be0daca74e0e0
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-07-16 14:20:13 +0530

    Update basic1.js

--- basic1.js
+++ basic1.js
@@ -809,7 +809,6 @@ var b = "foo";
 var c = false;
 var d = a && b || c ? c || b ? a : c && b : a;         // we can resolve this by ((a && b) || c) ? ((c || b) ? a : (c && b)) : a
 console.log(d); // 42
-
 */
 
 // Find the output of below code
