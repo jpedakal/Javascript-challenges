dd0253ad2680e363e9fba3d3edbc48de1217cf27
Jayakrishna Pedakalam <40006634+jpedakal@users.noreply.github.com>
2020-07-17 17:27:34 +0530

    Update basic1.js

--- basic1.js
+++ basic1.js
@@ -882,6 +882,11 @@ foo(); // ReferenceError: b is not defined
 */
 
 // Find the output of below code
-function foo(){
-
+function foo(a = 38, b = a + 2) {
+    console.log(a, b);
 }
+//foo(); // 38 40
+//foo(undefined);
+//foo(5); // 5 7
+//foo(void 0, 7); // 38 7
+foo(null); // null 2
